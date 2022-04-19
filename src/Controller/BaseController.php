e2947a391e8a9a14599d0caa23ac30e6cc78745d
cuongtobi <cuongvuong299@gmail.com>
2022-04-19 14:58:09 +0700

    :hammer: Implement HttpHandler

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,11 +1,15 @@
 <?php
 namespace App\Controller;
 
+use App\Handlers\HttpHandler;
+
 class BaseController {
     protected $id;
     protected $requestMethod;
+    protected $httpHandler;
 
     public function __construct($requestMethod, $id = null) {
+        $this->httpHandler = new HttpHandler;
         $this->requestMethod = $requestMethod;
         $this->id = $id;
     }
@@ -29,11 +33,11 @@ class BaseController {
                 $response = $this->delete($this->id);
                 break;
             default:
-                $response = $this->notFoundResponse();
+                $response = $this->httpHandler->notFoundResponse();
                 break;
         }
 
-        header($this->httpHeader($response['statusCode']));
+        header(HttpHandler::HTTP_HEADERS[$response['statusCode']]);
 
         if ($response['body']) {
             echo json_encode($response['body']);
@@ -41,87 +45,22 @@ class BaseController {
     }
 
     protected function index() {
-        return $this->methodNotAllowedResponse();
+        return $this->httpHandler->methodNotAllowedResponse();
     }
 
     protected function show($id) {
-        return $this->methodNotAllowedResponse();
+        return $this->httpHandler->methodNotAllowedResponse();
     }
 
     protected function create() {
-        return $this->methodNotAllowedResponse();
+        return $this->httpHandler->methodNotAllowedResponse();
     }
 
     protected function update() {
-        return $this->methodNotAllowedResponse();
+        return $this->httpHandler->methodNotAllowedResponse();
     }
 
     protected function delete() {
-        return $this->methodNotAllowedResponse();
-    }
-
-    protected function httpHeader($status) {
-        $http = array(
-            100 => 'HTTP/1.1 100 Continue',
-            101 => 'HTTP/1.1 101 Switching Protocols',
-            200 => 'HTTP/1.1 200 OK',
-            201 => 'HTTP/1.1 201 Created',
-            202 => 'HTTP/1.1 202 Accepted',
-            203 => 'HTTP/1.1 203 Non-Authoritative Information',
-            204 => 'HTTP/1.1 204 No Content',
-            205 => 'HTTP/1.1 205 Reset Content',
-            206 => 'HTTP/1.1 206 Partial Content',
-            300 => 'HTTP/1.1 300 Multiple Choices',
-            301 => 'HTTP/1.1 301 Moved Permanently',
-            302 => 'HTTP/1.1 302 Found',
-            303 => 'HTTP/1.1 303 See Other',
-            304 => 'HTTP/1.1 304 Not Modified',
-            305 => 'HTTP/1.1 305 Use Proxy',
-            307 => 'HTTP/1.1 307 Temporary Redirect',
-            400 => 'HTTP/1.1 400 Bad Request',
-            401 => 'HTTP/1.1 401 Unauthorized',
-            402 => 'HTTP/1.1 402 Payment Required',
-            403 => 'HTTP/1.1 403 Forbidden',
-            404 => 'HTTP/1.1 404 Not Found',
-            405 => 'HTTP/1.1 405 Method Not Allowed',
-            406 => 'HTTP/1.1 406 Not Acceptable',
-            407 => 'HTTP/1.1 407 Proxy Authentication Required',
-            408 => 'HTTP/1.1 408 Request Time-out',
-            409 => 'HTTP/1.1 409 Conflict',
-            410 => 'HTTP/1.1 410 Gone',
-            411 => 'HTTP/1.1 411 Length Required',
-            412 => 'HTTP/1.1 412 Precondition Failed',
-            413 => 'HTTP/1.1 413 Request Entity Too Large',
-            414 => 'HTTP/1.1 414 Request-URI Too Large',
-            415 => 'HTTP/1.1 415 Unsupported Media Type',
-            416 => 'HTTP/1.1 416 Requested Range Not Satisfiable',
-            417 => 'HTTP/1.1 417 Expectation Failed',
-            500 => 'HTTP/1.1 500 Internal Server Error',
-            501 => 'HTTP/1.1 501 Not Implemented',
-            502 => 'HTTP/1.1 502 Bad Gateway',
-            503 => 'HTTP/1.1 503 Service Unavailable',
-            504 => 'HTTP/1.1 504 Gateway Time-out',
-            505 => 'HTTP/1.1 505 HTTP Version Not Supported',
-        );
-
-        return $http[$status];
-    }
-
-    protected function notFoundResponse() {
-        return array(
-            'statusCode' => 404,
-            'body' => array(
-                'message' => 'Not Found.',
-            ),
-        );
-    }
-
-    protected function methodNotAllowedResponse() {
-        return array(
-            'statusCode' => 405,
-            'body' => array(
-                'message' => 'Method not allowed.',
-            ),
-        );
+        return $this->httpHandler->methodNotAllowedResponse();
     }
 }
