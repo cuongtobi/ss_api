4316b46c67b8a528b25e123c584642c2dfd93d8a
cuongtobi <cuongvuong299@gmail.com>
2022-04-19 14:03:04 +0700

    :sparkles: Implement BaseController

--- public/index.php
+++ public/index.php
@@ -5,11 +5,32 @@ use App\Controller\UserController;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,PATCH,DELETE");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$requestMethod = $_SERVER["REQUEST_METHOD"];
+$routes = array('users');
+$id = null;
 
-$controller = new UserController($requestMethod);
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+
+if (!isset($uri[1]) || empty($uri[1]) || !in_array($uri[1], $routes)) {
+    header("HTTP/1.1 404 Not Found");
+    exit;
+}
+
+if (isset($uri[2]) && !empty($uri[2])) {
+    $id = $uri[2];
+}
+
+$controllerClass = 'App\\Controller\\' . ucfirst(rtrim($uri[1], 's')) . 'Controller';
+
+if (!class_exists($controllerClass)) {
+    header("HTTP/1.1 404 Not Found");
+    exit;
+}
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$controller = new $controllerClass($requestMethod, $id);
 $controller->handle();
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,4 +2,23 @@
 namespace App\Controller;
 
 class UserController extends BaseController {
+    protected function index() {
+        return array(
+            'statusCode' => 200,
+            'body' => array(
+                array('id' => 1, 'name' => 'Name 1'),
+                array('id' => 2, 'name' => 'Name 2'),
+                array('id' => 3, 'name' => 'Name 3'),
+                array('id' => 4, 'name' => 'Name 4'),
+                array('id' => 5, 'name' => 'Name 5'),
+            ),
+        );
+    }
+
+    protected function show($id) {
+        return array(
+            'statusCode' => 200,
+            'body' => array('id' => $id, 'name' => 'Name ' . $id),
+        );
+    }
 }
